f97bb9719921dfdfaac115722c1e59e90e3e4eb7
wzr <thuwzr@gmail.com>
2013-07-17 09:39:30 +0800

    add findMulBall & getphoto

--- getPhoto.h
+++ getPhoto.h
@@ -20,20 +20,20 @@ static pthread_mutex_t ca_mutex;
 static struct net nst[2];
 static int photoCnt = 0;
 static int size = 0;
-static unsigned char raw_buff1[MAX_MJPEG_SIZE];
-static unsigned char* raw_buff;
+static unsigned char raw_buf1[MAX_MJPEG_SIZE];
+static unsigned char raw_buf2[MAX_MJPEG_SIZE];
 
 static void* myInit(void* arg);
 
 static void ptInit() {
-        pthread_t pt;
-        pthread_create(&pt, NULL, myInit, NULL);
-        usleep(2000);
+    pthread_t pt;
+    pthread_create(&pt, NULL, myInit, NULL);
+    usleep(2000);
 }
 
 static void* myInit(void* arg) {
 	pthread_mutex_init(&ca_mutex, NULL);
-	raw_buff = raw_buff1;
+	//raw_buff = raw_buff1;
 	int port1 = 8000, port2 = 8001;
 	nets_init(&nst[0], port1);
 	nets_init(&nst[1], port2);
@@ -43,8 +43,12 @@ static void* myInit(void* arg) {
         //printf("3\n");
         usleep(2000);
         pthread_mutex_lock(&ca_mutex);
-        if ((size = net_recv(raw_buff, MAX_MJPEG_SIZE, &(nst[1]))) <= 0) {
-            fprintf(stderr, "net_recv error\n");
+        if ((size = net_recv(raw_buf1, MAX_MJPEG_SIZE, &(nst[0]))) <= 0) {
+            fprintf(stderr, "net_recv1 error\n");
+            return (void*)0;
+        }
+        if ((size = net_recv(raw_buf2, MAX_MJPEG_SIZE, &(nst[1]))) <= 0) {
+            fprintf(stderr, "net_recv2 error\n");
             return (void*)0;
         }
         pthread_mutex_unlock(&ca_mutex);
@@ -52,41 +56,6 @@ static void* myInit(void* arg) {
         usleep(2000);
     }
 }
-
-/*
-int getInfo(int num) {
-	int size = 0;
-	if ((size = net_recv(raw_buff, MAX_MJPEG_SIZE, &(nst[num]))) <= 0) {
-		fprintf(stderr, "net_recv error\n");
-		return -1;
-	}
-	struct jpeg_decompress_struct* jpeg_decompressor = newDecompressor ( MAX_NET_WIDTH );
-
-	long rgbbuffersize = MAX_NET_WIDTH * MAX_NET_HEIGHT * 3;
-	unsigned char rgbbuffer[rgbbuffersize];
-
-	if (read_JPEG_buffer(jpeg_decompressor, raw_buff, size, rgbbuffer, rgbbuffersize, NULL, 0) != 1) {
-		fprintf(stderr, "\nerror while decoding jpeg files.\n");
-		if (isfatalerror()) {
-			fprintf(stderr, "\nwarning: fatal error occur. reconstructing decompress process.\n");
-			deleteDecompressor(jpeg_decompressor);
-			jpeg_decompressor = newDecompressor(MAX_NET_WIDTH);
-		}
-		return -1;
-	}
-
-	char dataName[300];
-	sprintf(dataName, "%d.dat", photoCnt, num);
-	photoCnt++;
-	FILE* p = fopen(dataName, "w");
-	fprintf(p, "%d %d\n", 320, 240);
-	long i;
-	for (i = 0; i < rgbbuffersize; i += 3)
-		fprintf(p, "%.2x %.2x %.2x\n", rgbbuffer[i], rgbbuffer[i + 1], rgbbuffer[i + 2]);
-	fclose(p);
-	return 0;
-}
-*/
 
 static void ptEnd() {
     net_close(&(nst[0]));
@@ -97,37 +66,44 @@ static void getPhoto() {
     //printf("1\n");
     usleep(2000);
 	int gsize = 0;
-	unsigned char tmpbuf[MAX_MJPEG_SIZE];
+	unsigned char tmpbuf[2][MAX_MJPEG_SIZE];
 
 	pthread_mutex_lock(&ca_mutex);
-	memcpy(tmpbuf, raw_buff, MAX_MJPEG_SIZE);
+	memcpy(tmpbuf1, raw_buf1, MAX_MJPEG_SIZE);
+	memcpy(tmpbuf2, raw_buf2, MAX_MJPEG_SIZE);
 	gsize = size;
 	pthread_mutex_unlock(&ca_mutex);
     //printf("2\n");
 	usleep(2000);
-	struct jpeg_decompress_struct* jpeg_decompressor = newDecompressor ( MAX_NET_WIDTH );
-	long rgbbuffersize = MAX_NET_WIDTH * MAX_NET_HEIGHT * 3;
-	unsigned char rgbbuffer[rgbbuffersize];
 
-	if (read_JPEG_buffer(jpeg_decompressor, tmpbuf, gsize, rgbbuffer, rgbbuffersize, NULL, 0) != 1) {
-		fprintf(stderr, "\nerror while decoding jpeg files.\n");
-		if (isfatalerror()) {
-			fprintf(stderr, "\nwarning: fatal error occur. reconstructing decompress process.\n");
-			deleteDecompressor(jpeg_decompressor);
-			jpeg_decompressor = newDecompressor(MAX_NET_WIDTH);
+	for (int i = 0; i != 2; ++i) {
+		struct jpeg_decompress_struct* jpeg_decompressor = newDecompressor ( MAX_NET_WIDTH );
+		long rgbbuffersize = MAX_NET_WIDTH * MAX_NET_HEIGHT * 3;
+		unsigned char rgbbuffer[rgbbuffersize];
+
+		if (read_JPEG_buffer(jpeg_decompressor, tmpbuf[i], gsize, rgbbuffer, rgbbuffersize, NULL, 0) != 1) {
+			fprintf(stderr, "\nerror while decoding jpeg files.\n");
+			if (isfatalerror()) {
+				fprintf(stderr, "\nwarning: fatal error occur. reconstructing decompress process.\n");
+				deleteDecompressor(jpeg_decompressor);
+				jpeg_decompressor = newDecompressor(MAX_NET_WIDTH);
+			}
+			return;
 		}
-		return;
-	}
 
-	char dataName[300];
-	sprintf(dataName, "./data/%d.dat", photoCnt);
-	photoCnt++;
-	FILE* p = fopen(dataName, "w");
-	fprintf(p, "%d %d\n", 320, 240);
-	long i;
-	for (i = 0; i < rgbbuffersize; i += 3)
-		fprintf(p, "%.2x %.2x %.2x\n", rgbbuffer[i], rgbbuffer[i + 1], rgbbuffer[i + 2]);
-	fclose(p);
+		char dataName[300];
+		if (i == 0)
+			sprintf(dataName, "./data/%d_l.dat", photoCnt);
+		else
+			sprintf(dataName, "./data/%d_r.dat", photoCnt++);
+		FILE* p = fopen(dataName, "w");
+		fprintf(p, "%d %d\n", 320, 240);
+		long i;
+		for (i = 0; i < rgbbuffersize; i += 3)
+			fprintf(p, "%.2x %.2x %.2x\n", rgbbuffer[i], rgbbuffer[i + 1], rgbbuffer[i + 2]);
+		fclose(p);
+	}
+	
 }
 
 #endif
